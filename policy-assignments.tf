40748a361747806b40119ac4f98d4f8ebe450e5a
Allan Taylor <allan@startmail.com>
2022-03-24 15:41:12 -0400

    Update policy-assignments.tf

--- policy-assignments.tf
+++ policy-assignments.tf
@@ -1,5 +1,27 @@
-resource "azurerm_policy_assignment" "policy_assignment" {
-  name                 = var.policy_name
-  scope                = "/subscriptions/${var.subscription}"
-  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/${policy_definition_id}"
+resource "azurerm_subscription_policy_assignment" "defender_for_cloud_policy_assignment" {
+  for_each             = {for subscription in var.subscriptions : subscription.name => subscription}
+  name                 = "${each.key} - ${var.policy_name}"
+  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/${var.policy_definition_id}"
+  subscription_id      = "/subscriptions/${each.value.id}"
+}
+
+resource "azurerm_security_center_subscription_pricing" "management_plans" {
+  for_each      = toset( var.management_enchanced_security_plans )
+  provider      = azurerm
+  tier          = "Standard"
+  resource_type = each.key
+}
+
+resource "azurerm_security_center_subscription_pricing" "microservices_plans" {
+  for_each      = toset( var.microservices_enchanced_security_plans )
+  provider      = azurerm
+  tier          = "Standard"
+  resource_type = each.key
+}
+
+resource "azurerm_security_center_subscription_pricing" "network_plans" {
+  for_each      = toset( var.network_enchanced_security_plans )
+  provider      = azurerm
+  tier          = "Standard"
+  resource_type = each.key
 }
