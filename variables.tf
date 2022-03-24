47e8459ab3fd6cb40fd95769810932307d3d7656
Allan Taylor <allan@startmail.com>
2022-03-24 15:42:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "subscription" {
+variable "subscriptions" {
   description = "Azure subscription to apply policy towards."
-  type        = string
+  type        = list
 }
 
 variable "policy_name" {
@@ -11,4 +11,22 @@ variable "policy_name" {
 variable "policy_definition_id" {
   description = "Azure policy definition id."
   type        = string
+}
+
+variable "management_enchanced_security_plans" {
+  description = "List of resource types to enable enchanced security plans.  Possible values are AppServices, ContainerRegistry, KeyVaults, KubernetesService, SqlServers, SqlServerVirtualMachines, StorageAccounts, VirtualMachines, Arm, OpenSourceRelationalDatabases, Containers and Dns."
+  type        = list
+  default     = null
+}
+
+variable "microservices_enchanced_security_plans" {
+  description = "List of resource types to enable enchanced security plans.  Possible values are AppServices, ContainerRegistry, KeyVaults, KubernetesService, SqlServers, SqlServerVirtualMachines, StorageAccounts, VirtualMachines, Arm, OpenSourceRelationalDatabases, Containers and Dns."
+  type        = list
+  default     = null
+}
+
+variable "network_enchanced_security_plans" {
+  description = "List of resource types to enable enchanced security plans.  Possible values are AppServices, ContainerRegistry, KeyVaults, KubernetesService, SqlServers, SqlServerVirtualMachines, StorageAccounts, VirtualMachines, Arm, OpenSourceRelationalDatabases, Containers and Dns."
+  type        = list
+  default     = null
 }
