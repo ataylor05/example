4c3a15dfbdbabd45564eea174a3a8c773d562add
Allan Taylor <allan@startmail.com>
2022-03-25 12:03:17 -0400

    Update variables.tf

--- modules/defender-for-cloud/variables.tf
+++ modules/defender-for-cloud/variables.tf
@@ -12,3 +12,9 @@ variable "policy_definition_id" {
   description = "Azure policy definition id."
   type        = string
 }
+
+variable "enchanced_security_plans" {
+  description = "List of resource types to enable enchanced security plans.  Possible values are AppServices, ContainerRegistry, KeyVaults, KubernetesService, SqlServers, SqlServerVirtualMachines, StorageAccounts, VirtualMachines, Arm, OpenSourceRelationalDatabases, Containers and Dns."
+  type        = list(any)
+  default     = null
+}
